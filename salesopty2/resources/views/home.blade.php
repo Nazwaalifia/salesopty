6bcaa7180c867c244aca72eb355ad546b1d9d9b3
mrw16 <rifqiwahyudi16@gmail.com>
2022-07-14 09:18:14 +0700

    Second Commit

--- salesopty2/resources/views/home.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.main')
-@section('content')
-    <section class="section">
-        <div class="section-header">
-            <h1>Dashboard</h1>
-        </div>
-    </section>
-@endsection
